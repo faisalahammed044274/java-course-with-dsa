56b06de0222afc6e506916a1e6a6541c323579b7
faisalahammed044274 <faisalahammed044274@gmail.com>
2024-09-04 18:02:06 +0600

    first commit

---
+++ src/Variable.java
@@ -0,0 +1,15 @@
+public class Variable {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 2;
+        int sum = a+b;
+        System.out.println("Sum of a and b is: "+sum);
+        int diff =  a-b;
+        System.out.println("Difference of a and b is: "+diff);
+        int multiply= a*b;
+        System.out.println("Multiple of a & b is :"+multiply);
+        int division = a/b;
+        System.out.println("Division of a & b is :"+division);
+         
+    }
+}
