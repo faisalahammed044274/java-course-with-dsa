be5e9a4eae3fe494f7c7f527d6c929cdf5e741aa
faisalahammed044274 <faisalahammed044274@gmail.com>
2024-09-04 18:33:49 +0600

    elseif like child

---
+++ src/ElseIf.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class ElseIf {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (a == b) {
+            System.out.println("A & B are equals.");
+    } else{
+ if (a > b) {
+    System.out.println("A is greater than B.");
+ } else {
+    System.out.println("A is lesser than B");
+ }
+    }
+}
+}
