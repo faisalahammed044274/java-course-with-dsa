2c6582d8447aba1abed30cfbf464eb20d6b48965
faisalahammed044274 <faisalahammed044274@gmail.com>
2024-09-05 13:14:18 +0600

    loops

---
+++ src/Loops.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class Loops {
+    public static void main(String args[]){
+        // for (int PrintHello =0; PrintHello<3; PrintHello++) {
+        //     System.out.println("Hello World");
+        // }
+
+        // for(int count= 1; count<=5; count++){
+        //     System.out.println(count);
+        // }
+        // for(int i =0;i <= 10; i++){
+        //     System.out.println(i);
+        // }
+
+        // int x = 1;
+
+        // while (x <= 8) {
+        //    System.out.println(x);
+        //    x++;
+        // }
+
+        // int myroot = 10;
+
+        // while (myroot <= 20) {
+        //     System.out.println(myroot);
+        //     myroot++;
+        // }
+
+
+        // int i = 5;
+
+        // do { 
+        //     System.out.println(i);
+        //     i++;
+        // } while (i < 11);
+
+// Scanner sc = new Scanner(System.in);
+// int n = sc.nextInt();
+
+// int sum = 0;
+// for (int i = 0; i<=n; i++){
+//     sum = sum + i;
+// }
+// System.out.println(sum);
+
+
+
+Scanner sc = new Scanner(System.in);
+int n = sc.nextInt();
+
+for(int i = 1; i <= 10; i++){
+    System.out.println(n * i);
+}
+
+
+}}
